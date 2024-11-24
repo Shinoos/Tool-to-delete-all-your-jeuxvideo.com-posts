b5c22e6c7a3067bfac1f94241dbc31135189fb92
Shinoos <42747370+Shinoos@users.noreply.github.com>
2024-11-24 03:39:18 +0100

    Update userscript.user.js v1.0.2
    Minor changes to avoid a popup with some userscript managers, and the button will only appear in your message history.

--- userscript.user.js
+++ userscript.user.js
@@ -2,8 +2,9 @@
 // @name         Tool-to-delete-all-your-jeuxvideo.com-posts
 // @description  Tool to delete all your posts on the jeuxvideo.com forums.
 // @author       Shinoos
-// @version      1.0.1
+// @version      1.0.2
 // @match        https://www.jeuxvideo.com/profil/*?mode=historique_forum
+// @connect      raw.githubusercontent.com
 // @updateURL    https://raw.githubusercontent.com/Shinoos/Tool-to-delete-all-your-jeuxvideo.com-posts/main/userscript.user.js
 // @downloadURL  https://raw.githubusercontent.com/Shinoos/Tool-to-delete-all-your-jeuxvideo.com-posts/main/userscript.user.js
 // @grant        GM_xmlhttpRequest
@@ -17,48 +18,52 @@
         const pageInfo = document.querySelector('.bloc-pagi-default');
         if (!pageInfo) return;
 
-        const buttonDiv = document.createElement('div');
-        const button = document.createElement('button');
-        button.textContent = "Supprimer tous les messages";
-        button.style.padding = "8px 16px";
-        button.style.fontSize = "14px";
-        button.style.cursor = "pointer";
-        button.style.fontWeight = "bold";
-        button.style.backgroundColor = "#2e3238";
-        button.style.color = "white";
-        button.style.border = "none";
-        button.style.borderRadius = "5px";
-        button.style.fontFamily = "Arial, sans-serif";
-        button.style.transition = "background-color 0.3s ease, transform 0.5s ease";
-        buttonDiv.appendChild(button);
-        pageInfo.insertBefore(buttonDiv, pageInfo.firstChild);
+        const headerPseudo = document.querySelector('.headerAccount__pseudo')?.textContent.trim();
+        const urlParams = new URLSearchParams(window.location.search);
+        const urlPseudo = urlParams.get('mode') === 'historique_forum' ? window.location.pathname.split('/')[2] : '';
 
-        button.addEventListener('mouseover', function() {
-            button.style.backgroundColor = "#353a42";
-
-
-            button.style.transform = "scale(1.05)";
-        });
-
-        button.addEventListener('mouseout', function() {
+        if (headerPseudo && urlPseudo && headerPseudo.toLowerCase() === urlPseudo.toLowerCase()) {
+            const buttonDiv = document.createElement('div');
+            const button = document.createElement('button');
+            button.textContent = "Supprimer tous mes messages";
+            button.style.padding = "8px 16px";
+            button.style.fontSize = "14px";
+            button.style.cursor = "pointer";
+            button.style.fontWeight = "bold";
             button.style.backgroundColor = "#2e3238";
-            button.style.transform = "scale(1)";
-        });
+            button.style.color = "white";
+            button.style.border = "none";
+            button.style.borderRadius = "5px";
+            button.style.fontFamily = "Arial, sans-serif";
+            button.style.transition = "background-color 0.3s ease, transform 0.5s ease";
+            buttonDiv.appendChild(button);
+            pageInfo.insertBefore(buttonDiv, pageInfo.firstChild);
 
-        button.addEventListener('mousedown', function() {
-            button.style.transform = "scale(0.95)";
-        });
+            button.addEventListener('mouseover', function() {
+                button.style.backgroundColor = "#353a42";
+                button.style.transform = "scale(1.05)";
+            });
 
-        button.addEventListener('mouseup', function() {
-            button.style.transform = "scale(1)";
-        });
+            button.addEventListener('mouseout', function() {
+                button.style.backgroundColor = "#2e3238";
+                button.style.transform = "scale(1)";
+            });
 
-        button.addEventListener('click', function() {
-            if (confirm('Êtes-vous sûr de vouloir lancer la suppression de tous vos messages ?')) {
-                executeDeleteScript();
-                button.style.display = 'none';
-            }
-        });
+            button.addEventListener('mousedown', function() {
+                button.style.transform = "scale(0.95)";
+            });
+
+            button.addEventListener('mouseup', function() {
+                button.style.transform = "scale(1)";
+            });
+
+            button.addEventListener('click', function() {
+                if (confirm('Êtes-vous sûr de vouloir lancer la suppression de tous vos messages ?')) {
+                    executeDeleteScript();
+                    button.style.display = 'none';
+                }
+            });
+        }
     }
 
     function executeDeleteScript() {
